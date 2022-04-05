4ebdc5b57196983cf87d84d98d533c156abe1991
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2022-04-05 23:52:39 +0500

    Commit

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("todo.urls")),
-    path('docs/', include_docs_urls(title='Todo Api')),
 ]
